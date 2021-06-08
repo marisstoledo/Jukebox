9b4c6bce22ee73cad9efdebcb3952d726b57188b
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-06-08 09:12:23 -0700

    Functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+var songs = ["good 4 u  By Olivia Rodrigo", "Promiscuous  By Nelly Furtado", "Pretty Boy Swag  By Soulja Boy", "Jukebox  By Kid Quill", "Deja Vu  By Post Malone"]
+
+function renderSong(){
+for (let i=0; i <= 4; i++){
+let songContainer = document.getElementById("Song");
+let p = document.createElement(p);
+songContainer.appendChild(p);
+}}
+renderSong();
