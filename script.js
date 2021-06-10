d4f5c278f8ebd66f24bd6d1129b69d8f3aa7d400
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-06-10 10:05:56 -0700

    final work

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-var songs = [{HTMLid: 'good 4 u  By Olivia Rodrigo'},{HTMLid: 'Promiscuous  By Nelly Furtado'}, {HTMLid: 'Pretty Boy Swag  By Soulja Boy'}, {HTMLid: 'Jukebox  By Kid Quill'}, {HTMLid: 'Deja Vu  By Post Malone'}];
+var songs = [
+    {HTMLid: 'good 4 u  By Olivia Rodrigo'},
+    {HTMLid: 'Promiscuous  By Nelly Furtado'}, 
+    {HTMLid: 'Pretty Boy Swag  By Soulja Boy'}, 
+    {HTMLid: 'Jukebox  By Kid Quill'}, 
+    {HTMLid: 'Deja Vu  By Post Malone'},
+    {HTMLid: 'Peaches  By Justin Bieber'},
+    {HTMLid: 'Blinding Lights  By The Weeknd'}, 
+    {HTMLid: 'SugarCrash  By ElyOtto'}, 
+    {HTMLid: 'Skechers  By DripReport'},
+    {HTMLid: 'Buss It  By Erica Banks'}, 
+    {HTMLid: 'Astronaut In The Ocean  By Masked Wolf'},
+    {HTMLid: 'Track Star  By Mooski'}, 
+    {HTMLid: 'Stuck In The Middle  By Tai Verdes'}, 
+    {HTMLid: 'oops!  By Yung Gravy'},
+    {HTMLid: 'DESPERADOS  By Miles'}];
+
 function renderSong(){
     let songContainer = document.getElementById("Song");
     let index = randomize();
